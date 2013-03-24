3f04bf1431dda5143c4dde09d5c5e10d6e02b7e2
dezm00nd <dezm00nd@gmail.com>
2013-03-25 01:19:29 +0300

    first

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ time1 = time.clock()
 X = [i for i in range(0, 100000)]
 time2 = time.clock()
 printTime(time1, time2)
+#test comment
 while X:
     front, X = X[0], X[1:]
     if front % 1000 == 0:
